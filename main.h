538fa288811d5450a04cc37869dc7a03717373af
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-07-21 21:11:02 +0000

    modified struct in main.h

--- main.h
+++ main.h
@@ -3,11 +3,21 @@
 
 int _printf(const char *format, ...);
 
-typedef struct formats
+
+/**
+ * struct format_specifier - structure defining data type of format specifier
+ * and the corresponding function
+ * @type: the format character
+ * @func: function pointer of thr corresponfing function
+ *
+ * Description:this helps to select the function
+ * that matches the provided specifier
+ */
+typedef struct format_specifier
 {
-	char specifier;
+	char type;
 	void (*func)(va_list);
-} form;
+} specifier;
 
 
 
