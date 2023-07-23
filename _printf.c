5003bdd40a4fbc4b3f61bd081af67742f36f20ed
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-07-23 18:15:24 +0100

    fixed betty

--- _printf.c
+++ _printf.c
@@ -13,10 +13,8 @@ int _printf(const char *format, ...)
 	va_list args;
 	int i = 0, j, count = 0;
 	specifier id[] = {
-		{'c', print_c}, {'o', print_o},
-		{'s', print_s}, {'u', print_u}, /* added u and o */
-		{'d', print_d}, {'i', print_i}, /* added i and d */
-		{'x', print_x}, {'X', print_X}, /* added x and X */
+		{'c', print_c}, {'o', print_o}, {'s', print_s}, {'u', print_u},
+		{'d', print_d}, {'i', print_i}, {'x', print_x}, {'X', print_X},
 		{'b', print_b}, {0, NULL}
 	};
 	va_start(args, format);
@@ -36,9 +34,7 @@ int _printf(const char *format, ...)
 			while (id[j].type != 0)
 			{
 				if (id[j].type == format[i])
-				{
 					count += id[j].func(args); /* corrected count */
-				}
 				j++;
 			}
 		}
